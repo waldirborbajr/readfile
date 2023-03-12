71ff7d237187693caae8b607a9dd69d80d0427f7
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-03-12 09:26:25 -0300

    chore: initial import

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let file_name = std::env::args().nth(1).expect("file name not passed");
+
+    let file = std::fs::read_to_string(file_name).expect("unable to read file");
+
+    file.lines().for_each(|line| {
+        if let Ok(value) = line.parse::<usize>() {
+            println!("{}", value)
+        } else {
+            println!("Not a number")
+        }
+    });
+}
