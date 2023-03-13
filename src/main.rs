251ae99497b332f4fb43f1b5ea1fd21e05bdbae2
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-03-13 08:45:00 -0300

    chore: splitting into module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+mod contentlib;
+
 fn main() {
     let file_name = std::env::args().nth(1).expect("file name not passed");
 
     let file = std::fs::read_to_string(file_name).expect("unable to read file");
+
+    println!("{}", contentlib::sayversion());
 
     file.lines().for_each(|line| {
         if let Ok(value) = line.parse::<usize>() {
