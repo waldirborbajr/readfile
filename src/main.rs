690403cf7c3d9a5e7af04f3e77e05b9c3549c4b3
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-03-13 09:56:49 -0300

    chore: trait

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 mod contentlib;
 use contentlib::sayversion;
 
+pub trait MyTrait {
+    fn sum(&self) -> u32;
+}
+
+struct MyStruct {}
+impl MyTrait for MyStruct {
+    fn sum(&self) -> u32 {
+        10
+    }
+}
+
 fn main() {
     let file_name = std::env::args().nth(1).expect("file name not passed");
 
     let file = std::fs::read_to_string(file_name).expect("unable to read file");
+
+    let my_stru = MyStruct {};
+
+    println!(" sum = {}", my_stru.sum());
 
     println!("{}", sayversion::sayversion());
 
